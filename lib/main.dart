193a46fd90981a32aa4d5fcd16cf69a1a766123c
tanguy-kabore <baowendnere.kabore@gmail.com>
2025-06-02 20:03:51 +0000

    Field controller add

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,11 @@ class FirstPage extends StatefulWidget {
 }
 
 class FirstPageState extends State<FirstPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _lastNameController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _comentController = TextEditingController();
+  final _markController = TextEditingController();
   List<Student> students = [
     Student(lastname: 'KABORE', firstname: 'Tanguy', mark: 20),
     Student(lastname: 'OUEDRAOGO', firstname: 'Ibrahim', mark: 10),
@@ -47,70 +52,124 @@ class FirstPageState extends State<FirstPage> {
     students.removeAt(index);
     setState(() {});
   }
-
+  void createStudent(){
+    if(_formKey.currentState!.validate() 
+    && _lastNameController.value.text !="" 
+    && _firstNameController.value.text !=""
+    && _comentController.value.text !=""
+    && _markController.value.text !=""){
+      students.add(Student(
+        lastname: _lastNameController.value.text, 
+        firstname: _firstNameController.value.text,
+        mark: int.parse(_markController.value.text),
+        comment: _comentController.value.text));
+    }else{
+      
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(10),
-    child: Center(
-      child: Column(children: [
-        Expanded(child: ListView.builder(
-          itemCount: students.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context){
-                  return Detail(student: students[index]);
-                }));
-              },
-              leading: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.black),
-                ),
-                child: CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  child: Image.asset(
-                    'assets/images/armoirie.png',
-                    width: 30,
-                    height: 30,
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Center(
+          child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: students.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return Detail(student: students[index]);
+                      }));
+                    },
+                    leading: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        child: Image.asset(
+                          'assets/images/armoirie.png',
+                          width: 30,
+                          height: 30,
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                        '${students[index].lastname} ${students[index].firstname}'),
+                    subtitle: Text('${students[index].mark ?? 0}'),
+                    trailing: IconButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                content: const Text(
+                                    'Voulez-vous vraiment supprimer ?'),
+                                actions: [
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text(
+                                        'Non',
+                                        style: TextStyle(color: Colors.red),
+                                      )),
+                                  TextButton(
+                                      onPressed: () {
+                                        deleteStudent(students, index);
+                                        Navigator.of(context).pop();
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(const SnackBar(
+                                                content:
+                                                    Text('Bien supprimé!')));
+                                      },
+                                      child: const Text(
+                                        'Oui',
+                                        style: TextStyle(color: Colors.green),
+                                      )),
+                                ],
+                              );
+                            });
+                      },
+                      icon: const Icon(Icons.delete),
+                    ),
+                  );
+                }),
+          ),
+          Form(
+            key: _formKey,
+              child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Nom'),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Prénom'),
+              ),
+              TextField(
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(labelText: 'Commentaire'),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: 'Note'),
+                    ),
                   ),
-                ),
-              ),
-              title: Text(
-                  '${students[index].lastname} ${students[index].firstname}'),
-              subtitle: Text('${students[index].mark ?? 0}'),
-              trailing: IconButton(
-                onPressed: () {
-                  showDialog(context: context, builder: (context){
-                    return AlertDialog(
-                      content: const Text('Voulez-vous vraiment supprimer ?'),
-                      actions: [
-                        TextButton(onPressed: (){
-                          Navigator.of(context).pop();
-                        }, child: const Text('Non', style: TextStyle(color: Colors.red),)),
-                        TextButton(onPressed: (){
-                          deleteStudent(students, index);
-                          Navigator.of(context).pop();
-                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Bien supprimé!')));
-                        }, child: const Text('Oui', style: TextStyle(color: Colors.green),)),
-                      ],
-                    );
-                  });
-                },
-                icon: const Icon(Icons.delete),
-              ),
-            );
-          }),),
-          Form(child: Column(children: [
-            TextField(decoration: InputDecoration(labelText: 'Nom'),),
-            TextField(decoration: InputDecoration(labelText: 'Prénom'),),
-            TextField(decoration: InputDecoration(labelText: 'Commentaire'),),
-            Row(children: [
-              Expanded(child: TextField(decoration: InputDecoration(labelText: 'Note'),),),
-              ElevatedButton(onPressed: (){}, child: const Text('Save'))
-            ],) 
-          ],))
-      ],)
-    ),);
+                  ElevatedButton(onPressed: () {}, child: const Text('Save'))
+                ],
+              )
+            ],
+          ))
+        ],
+      )),
+    );
   }
 }
